f85aa3397b5e4bc66990b1c0bd668c0e297e799e
LeandroSimioni <leandrojesimioni@gmail.com>
2025-11-17 19:02:42 -0300

    corrigindo

---
+++ JavaScript.TodoList.Frontend/script.js
@@ -0,0 +1,4 @@
+const elFiltro = document.getElementbyid("filtro")
+const elTarefas = document.getElementbyid("tarefas")
+const elNovaTarefaDescricao = document.getElementbyid("nova-tarefa")
+const elAdcionarNovaTarefa = document.getElementbyid("adicionar-nova-tarefa")
