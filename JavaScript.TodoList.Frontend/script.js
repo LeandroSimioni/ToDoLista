7b31a19d66fae4c3d93b0ac8551648f2d2786983
LeandroSimioni <leandrojesimioni@gmail.com>
2025-11-25 18:25:52 -0300

    atualizar

--- JavaScript.TodoList.Frontend/script.js
+++ JavaScript.TodoList.Frontend/script.js
@@ -3,7 +3,7 @@ const URL_API = "http://localhost:3000"
 const elFiltro = document.getElementbyid("filtro")
 const elTarefas = document.getElementbyid("tarefas")
 const elNovaTarefaDescricao = document.getElementbyid("nova-tarefa")
-const elAdcionarNovaTarefa = document.getElementbyid("adicionar-nova-tarefa")
+const elAdicionarNovaTarefa = document.getElementbyid("adicionar-nova-tarefa")
 
 async function carregarTarefas() {
     try {
